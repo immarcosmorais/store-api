f9daa439e0ae239d319a5c795e64712ac0802c95
Marcos Morais <marcosmorais.contact@gmail.com>
2023-02-20 14:24:36 -0300

    Added try catch when finalize connection

--- repositories/client.repository.js
+++ repositories/client.repository.js
@@ -2,17 +2,23 @@ import { connect } from "./db.js";
 
 async function create(client) {
   const conn = await connect();
-  const sql =
-    "INSERT INTO clients (name, cpf, phone, email, address) VALUES ($1, $2, $3, $4, $5) RETURNING *";
-  const values = [
-    client.name,
-    client.cpf,
-    client.phone,
-    client.email,
-    client.address,
-  ];
-  const res = await conn.query(sql, values);
-  return res.rows[0];
+  try {
+    const sql =
+      "INSERT INTO clients (name, cpf, phone, email, address) VALUES ($1, $2, $3, $4, $5) RETURNING *";
+    const values = [
+      client.name,
+      client.cpf,
+      client.phone,
+      client.email,
+      client.address,
+    ];
+    const res = await conn.query(sql, values);
+    return res.rows[0];
+  } catch (err) {
+    throw err;
+  } finally {
+    conn.release();
+  }
 }
 
 export default { create };
