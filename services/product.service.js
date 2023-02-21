92a37162cc30b76b1f8ece0d1c78e79f4d462d1b
Marcos Morais <marcosmorais.contact@gmail.com>
2023-02-21 11:08:46 -0300

    Added validation for supplier

--- services/product.service.js
+++ services/product.service.js
@@ -1,7 +1,17 @@
 import productRepository from "../repositories/product.repository.js";
+import supplierRepository from "../repositories/supplier.repository.js";
 
-async function create(client) {
-  return await productRepository.create(client);
+async function supplierExist(id) {
+  if (await supplierRepository.find(id)) {
+    return true;
+  }
+  throw new Error("There is not supplier with id informed");
+}
+
+async function create(product) {
+  if (await supplierExist(product.supplier_id)) {
+    return await productRepository.create(product);
+  }
 }
 
 async function all() {
@@ -16,8 +26,10 @@ async function remove(id) {
   await productRepository.remove(id);
 }
 
-async function update(client, id) {
-  return await productRepository.update(client, id);
+async function update(product, id) {
+  if (await supplierExist(product.supplier_id)) {
+    return await productRepository.update(product, id);
+  }
 }
 
 export default { create, all, find, remove, update };
